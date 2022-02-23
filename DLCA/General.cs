10141ef1ae34968182b1755eb849616dd8f4d669
vlhells <Vladislav5750@yandex.ru>
2022-02-23 17:39:11 +0300

    -2.

--- DLCA/General.cs
+++ DLCA/General.cs
@@ -14,7 +14,7 @@ namespace DLCA
 
             SpawnTheCell(d_of_cell, field, x, y);
 
-            Draw(field);
+            Render.Draw(field);
         }
 
         static void SpawnTheCell(int d_of_cell, int[,] field, int x, int y)
@@ -39,21 +39,6 @@ namespace DLCA
                     field[n, m] = 1;
                 }
                 r_of_cell--;
-            }
-        }
-
-        static void Draw(int[,] field)
-        {
-            for (int i = 0; i < field.GetLength(0); i++)
-            {
-                for (int j = 0; j < field.GetLength(1); j++)
-                {
-                    if (field[i, j] == 1)
-                        Console.ForegroundColor = ConsoleColor.Green;
-                    Console.Write(field[i, j]);
-                    Console.ResetColor();
-                }
-                Console.WriteLine();
             }
         }
     }
---
+++ DLCA/Render.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLCA
+{
+    internal class Render
+    {
+        public static void Draw(int[,] field)
+        {
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    if (field[i, j] == 1)
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(field[i, j]);
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
